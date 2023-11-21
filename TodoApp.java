a52774e27483048d3a9caa41283da7e9a874c27d
rafael-leivas <rafaelblsteam@gmail.com>
2023-11-20 21:06:59 -0300

    Inserindo dados no banco e testando

--- TodoApp.java
+++ TodoApp.java
@@ -1,21 +1,44 @@
+import java.util.List;
+
 public class TodoApp {
     
     public static void main(String[] args) {
         
         Matricula matricula = new Matricula(); // Criando um objeto Matricula
 
-        matricula.setId(1);
         matricula.setNome("João da Silva");
         matricula.setDataNascimento("19/04/1990");
         matricula.setEmail("joao@example.com");
-        matricula.setEndereco("Rua Exemplo, 123");
+        matricula.setEndereco("Rua da Perdição, 123");
         matricula.setCep("12345678"); 
         matricula.setTelefone("123456789");
         matricula.setUser("joao123");
         matricula.setPassword("senha123");
-        matricula.setCurso("Ciência da Computação");
-        matricula.setObservacao("Aluno regular");
+        matricula.setCurso("ADS");
+        matricula.setObservacao("Sou bom de mais");
 
-        System.out.println(matricula.toString());
+        MatriculaStore.inserir(matricula);
+
+        matricula.setNome("FILIPINHO");
+        matricula.setDataNascimento("19/04/1990");
+        matricula.setEmail("FILIPINHO@example.com");
+        matricula.setEndereco("Rua da Perdição, 123");
+        matricula.setCep("12345678"); 
+        matricula.setTelefone("123456789");
+        matricula.setUser("joao123");
+        matricula.setPassword("senha123");
+        matricula.setCurso("ADS");
+        matricula.setObservacao("Sou bom de mais");
+
+        MatriculaStore.inserir(matricula);
+
+        List<Matricula> matriculas = MatriculaStore.listar();
+for (Matricula m : matriculas) {
+    System.out.println(m.toString());
+}
+
+
+
+        //System.out.println(matricula.toString());
     }
 }
